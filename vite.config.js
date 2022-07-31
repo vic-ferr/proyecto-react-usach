905d5a62584fb94ed67eb1b4f22434ae5ba1b529
Victor Fernandez <victorhf2014@gmail.com>
2022-07-31 00:24:32 -0400

    corrigo base en vite confi

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://vic-ferr.github.io/proyecto-react-usach/",
+  base: "/priyecto-usach/",
   plugins: [react()],
 });
